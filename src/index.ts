273abd22290c434ea1cc0f5f6ffc85cfea86326a
ThomasGrare <thomasgrare64@gmail.com>
2025-12-10 10:14:28 +0100

    ci: eslint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 // Export all components
 export { Button } from './components/Button';
 export type { ButtonProps } from './components/Button/Button.types';
-
-// Export styles
-export './components/Button/Button.css'
