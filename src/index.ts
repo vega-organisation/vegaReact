2a4a099409631951868a3c682dbe9d7b34cdbfca
Hardi99 <harditabuna@gmail.com>
2026-02-25 14:46:09 +0100

    fix: declutter working

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export { Button } from './components/Button';
 export type { ButtonProps } from './components/Button/Button.types';
+export { Card } from './components/Card';
+export type { CardProps } from './components/Card/Card.types';
 export { InputText } from './components/InputText';
 export type { InputTextProps } from './components/InputText/InputText.types';
 export { PhoneNumberInput } from './components/PhoneNumberInput';
